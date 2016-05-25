2f3a9ce1c8e44a8fdc205762496c7bc9018ea299
Jason Lowe-Power <jason@lowepower.com>
2016-05-24 20:49:41 -0500

    Added lots of formatting options and a Link class

--- cvCreator.py
+++ cvCreator.py
@@ -1,5 +1,14 @@
 from dominate.tags import *
 
+class Link(object):
+
+    def __init__(self, target, text):
+        self.target = target
+        self.text = text
+
+    def format(self):
+        return a(text, href = target)
+
 class BaseItem(object):
     fields = ['title', 'authors', 'date', 'location', 'details', 'links',
               'isInvited']
@@ -15,16 +24,59 @@ class BaseItem(object):
             setattr(self, name, '')
 
     def formatTitle(self):
-        if not title:
-            return ''
-        return h2(title)
+        if not self.title: return ''
+        if self.isInvited:
+            t = span('Invited', cls = 'invited')
+        else:
+            t = ''
+        return div(h2(t, self.title), cls = 'item-title')
 
+    def formatDate(self):
+        if not self.date: return ''
+        return div(self.date, cls = 'item-date')
+
+    def formatLocation(self):
+        if not self.location: return ''
+        return div(self.location, cls = 'item-location')
+
+    def formatLinks(self):
+        if not self.links: return ''
+        tmp = div(cls = 'item-links')
+        for link in self.links:
+            tmp.add(a(span(link.text, cls = 'item-link', __inline = True),
+                      href = link.target))
+        return tmp
+
+    def formatDetails(self):
+        if not self.details: return ''
+        return div([p(line) for line in self.details.split('\n')], cls = 'item-details')
+
+    def formatAuthors(self):
+        if not self.authors: return ''
+        tmp = div(cls = 'item-authors')
+        for i,author in enumerate(self.authors):
+            if i == len(authors) - 1:
+                comma = ''
+            else:
+                comma = ', '
+            if author.startswith('Tiffany') or author.startswith('Jason'):
+                tmp.add(span(author, cls = 'author-me'), comma)
+            else:
+                tmp.add(author + comma)
+        return tmp
 
 class PublicationItem(BaseItem):
     pass
 
 class PresenationItem(BaseItem):
-    pass
+
+    def formatTitle(self):
+        if not self.title: return ''
+        if self.isInvited:
+            t = span('Invited talk', cls='invited')
+        else:
+            t = ''
+        return div(h2(t, self.title), cls='title')
 
 class TeachingItem(BaseItem):
     pass
